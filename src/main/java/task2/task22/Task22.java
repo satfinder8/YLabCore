623df38296ffecaf12fc0e93fc352822443e1718
satfinder8 <satfinder8@gmail.com>
2022-09-11 18:06:21 +0300

    Task 2.2 - find two summands in array

---
+++ src/main/java/task2/task22/Task22.java
@@ -0,0 +1,43 @@
+package task2.task22;
+
+import java.util.Arrays;
+
+public class Task22 {
+
+    public static void main(String[] args) {
+
+        findSummands(new int[]{3, 4, 2, 7}, 10);
+        findSummands(new int[]{5, 4, 5, 5, 5}, 10);
+        findSummands(new int[]{0, 0, 0, 0}, 10);
+        findSummands(new int[]{10}, 10);
+        findSummands(null, 10);
+
+    }
+
+    private static void findSummands(int[] input, int sum) {
+
+        boolean isResult = false;
+        if (input == null) {
+            System.out.println("Отсутствует массив для анализа");
+        } else if (input.length < 2) {
+            System.out.println("Массив должен содержать не менее 2-ух элементов");
+        } else {
+            Arrays.sort(input);
+            for (int i = 0, j = input.length - 1; i < j; ) {
+                if (input[i] + input[j] == sum) {
+                    System.out.println("[" + input[i] + "," + input[j] + "]");
+                    isResult = true;
+                    break;
+                } else if (input[i] + input[j] > sum) {
+                    j--;
+                } else {
+                    i++;
+                }
+            }
+            if (isResult == false) {
+                System.out.println("Массив не содержит подходящей пары элементов");
+            }
+        }
+    }
+
+}
